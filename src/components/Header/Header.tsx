ec5f2ef6b47206af19ed5b97ba6427bebb1a7589
sscodes <sanketcrj@gmail.com>
2024-05-26 04:04:01 +0530

    responsive header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -6,13 +6,13 @@ import { pt_mono_400 } from '@/helpers/Fonts';
 const Header = () => {
   return (
     <nav className={`${pt_mono_400.className} grid grid-cols-6 ${styles.header} text-lg`}>
-      <div></div>
-      <div className='grid col-span-2 border-b-2 border-b-stone-400'>
-        <div className='flex xl:ml-7 items-center font-bold text-4xl'>
+      <div className='hidden 2xl:grid 2xl:col-span-1'></div>
+      <div className='grid col-span-6 lg:col-span-2 2xl:col-span-2 border-b-2 border-b-stone-400'>
+        <div className='flex ml-7 items-center justify-center lg:justify-start font-bold text-4xl'>
           <b className='cursor-pointer'>sscodes</b>
         </div>
       </div>
-      <div className='grid col-span-2 grid-cols-5 border-b-2 border-b-stone-400'>
+      <div className='hidden lg:grid lg:col-span-4 2xl:col-span-2 grid-cols-5 border-b-2 border-b-stone-400'>
         <div className='flex justify-center items-center'>
           <b className='cursor-pointer hover:text-sky-700'>Projects</b>
         </div>
@@ -29,7 +29,7 @@ const Header = () => {
           <ToggleButton />
         </div>
       </div>
-      <div></div>
+      <div className='hidden 2xl:grid  2xl:col-span-1'></div>
     </nav>
   );
 };
